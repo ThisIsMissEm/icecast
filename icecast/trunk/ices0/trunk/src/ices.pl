20229866b12b30173ed4ebb2b67a689f3ed78564
chad <chad@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2000-07-19 23:54:42 +0000

    Sample perl script, with api stubs.
    We've discovered the there is a flaw in perl.c returning strings. I'll fix
    that next.

---
+++ icecast/trunk/ices0/trunk/src/ices.pl
@@ -0,0 +1,17 @@
+
+sub ices_perl_initialize {
+	print "help me! I'm falling!\n";
+}
+
+sub ices_perl_shutdown {
+	print "i'm melting!\n";
+}
+
+sub ices_perl_get_next {
+	print "trying to die\n";
+	return "/home/chad/music/04_beatles.mp3";
+}
+
+sub ices_perl_get_lineno {
+	return 1;
+}
