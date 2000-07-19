3ed3a388fe7cb6fb5bfb3dfacf012ea314ce45f1
alex <alex@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2000-07-19 19:33:41 +0000

    Compability fixes between libxml 1 and 2. xml-config is just stupid like hell...

--- icecast/trunk/ices0/trunk/src/xml/parse.c
+++ icecast/trunk/ices0/trunk/src/xml/parse.c
@@ -21,8 +21,13 @@
 #include "definitions.h"
 
 #ifdef HAVE_LIBXML
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
+# ifdef HAVE_LIBXML_PARSER_H
+#  include <libxml/xmlmemory.h>
+#  include <libxml/parser.h>
+# else
+#  include <gnome-xml/parser.h>
+#  include <gnome-xml/parser.h>
+# endif
 
 /* Private function declarations */
 static int ices_xml_parse_file (const char *configfile, ices_config_t *ices_config);
@@ -31,6 +36,8 @@ static void ices_xml_parse_playlist_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePt
 static void ices_xml_parse_execution_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_config_t *ices_config);
 static void ices_xml_parse_server_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_config_t *ices_config);
 static void ices_xml_parse_stream_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_config_t *ices_config);
+static xmlNodePtr ices_xml_get_root_element (xmlDocPtr doc);
+static xmlNodePtr ices_xml_get_children_node (xmlNodePtr cur);
 #endif
 
 /* Global function definitions */
@@ -40,20 +47,26 @@ ices_xml_parse_config_file (ices_config_t *ices_config, const char *configfile)
 #ifndef HAVE_LIBXML
 	ices_log_error ("XML Parser cannot be run, because LIBXML was not found at compile time.");
 	return -1;
-}
 #else
+
 	char namespace[1024];
-
-	if (!ices_util_verify_file (configfile)) {
-		ices_log_error ("XML Parser Error: Could not open configfile. [%s]  Error: [%s]", configfile, ices_util_strerror (errno, namespace, 1024));
-		return 0;
-	}
-
+	
+/* Wonder what idjit came up with this...  */
+#ifdef LIBXML_TEST_VERSION
+	LIBXML_TEST_VERSION
+#endif
+		if (!ices_util_verify_file (configfile)) {
+			ices_log_error ("XML Parser Error: Could not open configfile. [%s]  Error: [%s]", configfile, ices_util_strerror (errno, namespace, 1024));
+			return 0;
+		}
+	
 	ices_xml_parse_setup_defaults ();
-
+	
 	return ices_xml_parse_file (configfile, ices_config);
+#endif
 }
 
+#ifdef HAVE_LIBXML
 /* I hope you can tell this is my first try at xml and libxml :) */
 static int
 ices_xml_parse_file (const char *configfile, ices_config_t *ices_config)
@@ -66,13 +79,13 @@ ices_xml_parse_file (const char *configfile, ices_config_t *ices_config)
 		ices_log_error ("XML Parser: Error while parsing %s", configfile);
 		return 0;
 	}
-
-	if (!(cur = xmlDocGetRootElement (doc))) {
+	
+	if (!(cur = ices_xml_get_root_element (doc))) {
 		ices_log_error ("XML Parser: Empty documenent %s", configfile);
 		xmlFreeDoc (doc);
 		return 0;
 	}
-
+	
 	if (!(ns = xmlSearchNsByHref (doc, cur, (unsigned char *)"http://www.icecast.org/projects/ices"))) {
 		ices_log_error ("XML Parser: Document of invalid type, no ices namespace found");
 		xmlFreeDoc (doc);
@@ -86,11 +99,13 @@ ices_xml_parse_file (const char *configfile, ices_config_t *ices_config)
 	}
 
 	/* Tree traversal */
-	cur = cur->xmlChildrenNode;
+	cur = ices_xml_get_children_node (cur);
 
+#ifdef HAVE_LIBXML_PARSER_H
 	while (cur && xmlIsBlankNode (cur)) {
 		cur = cur->next;
 	}
+#endif
 
 	if (cur == NULL) {
 		/* Right type of document, but no configuration, guess that is ok */
@@ -101,13 +116,13 @@ ices_xml_parse_file (const char *configfile, ices_config_t *ices_config)
 	while (cur != NULL) {
 
 		if (strcmp (cur->name, "Server") == 0) {
-			ices_xml_parse_server_nodes (doc, ns, cur->xmlChildrenNode, ices_config);
+			ices_xml_parse_server_nodes (doc, ns, ices_xml_get_children_node (cur), ices_config);
 		} else if (strcmp (cur->name, "Stream") == 0) {
-			ices_xml_parse_stream_nodes (doc, ns, cur->xmlChildrenNode, ices_config);
+			ices_xml_parse_stream_nodes (doc, ns, ices_xml_get_children_node (cur), ices_config);
 		} else if (strcmp (cur->name, "Playlist") == 0) {
-			ices_xml_parse_playlist_nodes (doc, ns, cur->xmlChildrenNode, ices_config);
+			ices_xml_parse_playlist_nodes (doc, ns, ices_xml_get_children_node (cur), ices_config);
 		} else if (strcmp (cur->name, "Execution") == 0) {
-			ices_xml_parse_execution_nodes (doc, ns, cur->xmlChildrenNode, ices_config);
+			ices_xml_parse_execution_nodes (doc, ns, ices_xml_get_children_node (cur), ices_config);
 		} else {
 			ices_log ("Unknown Node: %s", cur->name);
 		}
@@ -125,17 +140,17 @@ ices_xml_parse_stream_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_co
 {
 	while (cur != NULL) {
 		if (strcmp (cur->name, "Name") == 0) {
-			ices_config->name = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->name = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Genre") == 0) {
-			ices_config->genre = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->genre = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Description") == 0) {
-			ices_config->description = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->description = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "URL") == 0) {
-			ices_config->url = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->url = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Bitrate") == 0) {
-			ices_config->bitrate = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->bitrate = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Public") == 0) {
-			ices_config->ispublic = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->ispublic = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else {
 			ices_log ("Unknown Stream keyword: %s", cur->name);
 		}
@@ -149,17 +164,17 @@ ices_xml_parse_server_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_co
 {
 	while (cur != NULL) {
 		if (strcmp (cur->name, "Port") == 0) {
-			ices_config->port = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->port = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Hostname") == 0) {
-			ices_config->host = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->host = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Mountpoint") == 0) {
-			ices_config->mount = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->mount = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Password") == 0) {
-			ices_config->password = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->password = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Dumpfile") == 0) {
-			ices_config->dumpfile = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->dumpfile = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Protocol") == 0) {
-			unsigned char *str = xmlNodeListGetString (doc, cur->xmlChildrenNode, 1);
+			unsigned char *str = xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1);
 			if (str && (str[0] == 'i' || str[0] == 'I'))
 				ices_config->header_protocol = icy_header_protocol_e;
 			else
@@ -177,13 +192,13 @@ ices_xml_parse_execution_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices
 {
 	while (cur != NULL) {
 		if (strcmp (cur->name, "Background") == 0) {
-			ices_config->daemon = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->daemon = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Verbose") == 0) {
-			ices_config->verbose = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->verbose = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Base_Directory") == 0) {
-			ices_config->base_directory = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->base_directory = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Reencode") == 0) {
-			int res = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			int res = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 
 #ifndef HAVE_LIBLAME
 			if (res == 1) {
@@ -207,9 +222,9 @@ ices_xml_parse_playlist_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_
 {
 	while (cur != NULL) {
 		if (strcmp (cur->name, "Randomize") == 0) {
-			ices_config->randomize_playlist = atoi ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->randomize_playlist = atoi ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Type") == 0) {
-			unsigned char *str = xmlNodeListGetString (doc, cur->xmlChildrenNode, 1);
+			unsigned char *str = xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1);
 			if (str && (strcmp (str, "python") == 0))
 				ices_config->playlist_type = ices_playlist_python_e;
 			else if (str && (strcmp (str, "perl") == 0))
@@ -217,9 +232,9 @@ ices_xml_parse_playlist_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_
 			else
 				ices_config->playlist_type = ices_playlist_builtin_e;
 		} else if (strcmp (cur->name, "File") == 0) {
-			ices_config->playlist_file = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->playlist_file = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else if (strcmp (cur->name, "Module") == 0) {
-			ices_config->interpreter_file = ices_util_strdup ((char *)xmlNodeListGetString (doc, cur->xmlChildrenNode, 1));
+			ices_config->interpreter_file = ices_util_strdup ((char *)xmlNodeListGetString (doc, ices_xml_get_children_node(cur), 1));
 		} else {
 			ices_log ("Unknown playlist keyword: %s", cur->name);
 		}
@@ -229,10 +244,33 @@ ices_xml_parse_playlist_nodes (xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur, ices_
 
 }
 
+static xmlNodePtr
+ices_xml_get_children_node (xmlNodePtr cur)
+{
+	/* libxml version 2*/
+#ifdef HAVE_LIBXML_PARSER_H
+	return cur->xmlChildrenNode;
+#else
+	return cur->childs;
+#endif
+}
+
+static xmlNodePtr
+ices_xml_get_root_element (xmlDocPtr doc)
+{
+#ifdef HAVE_LIBXML_PARSER_H
+	return xmlDocGetRootElement (doc);
+#else
+	return doc->root;
+#endif
+}
+
 static void
 ices_xml_parse_setup_defaults ()
 {
+#ifdef HAVE_LIBXML_PARSER_H
 	xmlKeepBlanksDefault(0);
+#endif
 }
 #endif
 			
