cfe948a3dc8dba3eebf0a7f51c7d1da2eb0df84f
chad <chad@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2000-07-21 00:56:35 +0000

    (medium)
    Did a bunch of cleanup in preperation of another release
    -added comments!
    -catching perl_init errors now, shutting things down nicely, rather
    than assuming everything is 'okay' and continuing to run.
    -the perl source file is now called ices.pm now, as is fitting, still
    hardcoded however, unlike the python script filename. (see also -M option)

--- icecast/trunk/ices0/trunk/src/interpreter/perl.c
+++ icecast/trunk/ices0/trunk/src/interpreter/perl.c
@@ -28,54 +28,73 @@
 
 static PerlInterpreter *my_perl;
 
+/* most of the following is almost ripped straight out of 'man perlcall' or 'man perlembed' 
+ *
+ * my_perl is left resident, and we do not reload the perl module file when it changes.
+ * shutdown() will clean up anything allocated at this point
+ */
+
 static void
 interpreter_perl_init ()
 {
-	static char *my_argv[] = {"", "ices.pl"};
+	static char *my_argv[] = {"", "ices.pm"}; 	/* dummy arguments, hardcoded pearl module */
 	
-	ices_log_debug ("Importing Perl module...");
+	ices_log_debug ("Importing Perl module ices.pm:");
 
 	if((my_perl = perl_alloc()) == NULL) {
 		ices_log_debug ("perl_alloc() error: (no memory!*");
+		ices_setup_shutdown();
 		return;
 	}
 	perl_construct(my_perl);
 
 	if (perl_parse(my_perl, NULL, 2, my_argv, NULL)) {
 		ices_log_debug ("perl_parse() error: parse problem");
+		ices_setup_shutdown();
 	}
 }
 
+/* cleanup time! */
 static void
 interpreter_perl_shutdown ()
 {
-	perl_destruct(my_perl);
-	perl_free(my_perl);
+	if (my_perl != NULL){
+		perl_destruct(my_perl);
+		perl_free(my_perl);
+	}
 }
 
+
+/*
+ *  Here be magic...
+ *  man perlcall gave me the following steps, to be
+ *  able to handle getting return values from the
+ *  embedded perl calls.
+ */	
 void *
 interpreter_perl_eval_function (char *functionname)
 {
-	dSP;
+	dSP;				/* initialize stack pointer      */
 	int retcount = 0;
 	char *retstr;
 	
 	ices_log_debug ("Interpreting [%s]", functionname);
 	
-	ENTER;
-	SAVETMPS;
-	PUSHMARK(SP);
-	PUTBACK;
+	ENTER;				/* everything created after here */
+	SAVETMPS;			/* ...is a temporary variable.   */
+	PUSHMARK(SP);			/* remember the stack pointer    */
+	PUTBACK;			/* make local stack pointer global */
 	
-	retcount = perl_call_pv(functionname, G_SCALAR);
+	retcount = perl_call_pv(functionname, G_SCALAR);	/* make the call */
 	
-	retstr = ices_util_strdup (POPp);
+	SPAGAIN;			/* refresh stack pointer         */
 	
+	/* we're calling strdup here, free() this later! */
+	retstr = ices_util_strdup (POPp);/* pop the return value from stack */        
 	ices_log_debug ("perl [%s] returned %d values, last [%s]", functionname, retcount, retstr);
 	
-	SPAGAIN;
-	FREETMPS;
-	LEAVE;
+	FREETMPS;			/* free that return value        */
+	LEAVE;				/* ...and the XPUSHed "mortal" args.*/
 	
 	ices_log_debug ("Done interpreting [%s]", functionname);
 	
