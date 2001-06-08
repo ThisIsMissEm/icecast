c53829917085c8a59bd5d0529b381299c01fea03
brendan <brendan@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-06-08 22:25:55 +0000

    Updated for the new LAME driver API (using 3.89a6). You need LAME as of today's CVS because of the streamlining of ices_reencode_reset.
    Handle shutdown properly.

--- icecast/trunk/ices0/trunk/src/reencode.c
+++ icecast/trunk/ices0/trunk/src/reencode.c
@@ -1,6 +1,7 @@
 /* reencode.c
  * - Functions for reencoding in ices
  * Copyright (c) 2000 Alexander Haväng
+ * Copyright (c) 2001 Brendan Cully
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,7 +27,7 @@
 extern ices_config_t ices_config;
 
 static int ices_reencode_initialized = 0;
-static lame_global_flags ices_reencode_flags;
+static lame_global_flags* ices_reencode_flags;
 static short int right[4096 * 30]; /* Probably overkill like hell, can someone calculate a better value please? */
 static short int left[4096 * 30];
 
@@ -38,55 +39,47 @@ static short int left[4096 * 30];
 void
 ices_reencode_initialize (void)
 {
-	char version[21];
-
 	if (!ices_config.reencode) 
 		return;
 
-	if (lame_init (&ices_reencode_flags) == -1) {
+	if (! (ices_reencode_flags = lame_init ())) {
 		ices_log ("Error: initialization of liblame failed!");
 		ices_setup_shutdown ();
 	}
 
-	if (lame_decode_init () == -1) {
-		ices_log ("Error: initialization of liblame's decoder failed!");
-		ices_setup_shutdown ();
+	lame_set_brate (ices_reencode_flags, ices_config.bitrate);
+	if (ices_config.out_numchannels != -1)
+		lame_set_num_channels (ices_reencode_flags, ices_config.out_numchannels);
+	if (ices_config.out_samplerate != -1)
+		lame_set_out_samplerate (ices_reencode_flags, ices_config.out_samplerate);
+
+	if (lame_init_params (ices_reencode_flags) == -1) {
+		ices_log ("Error: lame_init_params() failed!");
 	}
 
-	/* This is the bitrate we want the stream to be, specified by the
-	 * user with -b or in the config file */
-	ices_reencode_flags.brate = ices_config.bitrate;
-
-	if (lame_init_params (&ices_reencode_flags) == -1) {
-		ices_log ("Error: Second stage init of lame failed!");
-		ices_setup_shutdown ();
-	}
-
-	lame_version (&ices_reencode_flags, version);
-
-	ices_log_debug ("Initializing lame version %s\n", version);
-	ices_reencode_initialized = 0;
+	ices_log_debug ("Initializing lame version %s\n", get_lame_version ());
+	ices_reencode_initialized = 1;
 }
 
 /* Tell the reencoder what number of channels the current song has */
 void
 ices_reencode_set_channels (int channels)
 {
-	ices_reencode_flags.num_channels = channels;
+	ices_reencode_flags->num_channels = channels;
 }
 
 /* Tell the reencoder what sample rate the current song is */
 void
 ices_reencode_set_sample_rate (int samplerate)
 {
-	ices_reencode_flags.in_samplerate = samplerate;
+	ices_reencode_flags->in_samplerate = samplerate;
 }
 
 /* What mode is the current song in? */
 void
 ices_reencode_set_mode (int mode)
 {
-	ices_reencode_flags.mode = mode;
+	ices_reencode_flags->mode = mode;
 }
 
 /* For each song, reset the liblame engine, otherwize it craps out if
@@ -94,24 +87,9 @@ ices_reencode_set_mode (int mode)
 void
 ices_reencode_reset (void) 
 {
-	if (lame_init (&ices_reencode_flags) == -1) {
-		ices_log ("Error: initialization of liblame failed!");
-		ices_setup_shutdown ();
-	}
-
 	if (lame_decode_init () == -1) {
 		ices_log ("Error: initialization of liblame's decoder failed!");
 		ices_setup_shutdown ();
-	}
-
-	ices_reencode_flags.brate = ices_config.bitrate;
-	if (ices_config.out_numchannels != -1)
-		ices_reencode_flags.num_channels = ices_config.out_numchannels;
-	if (ices_config.out_samplerate != -1)
-		ices_reencode_flags.out_samplerate = ices_config.out_samplerate;
-
-	if (lame_init_params (&ices_reencode_flags) == -1) {
-		ices_log ("Error: lame_init_params() failed!");
 	}
 }
 
@@ -119,8 +97,8 @@ ices_reencode_reset (void)
 void
 ices_reencode_shutdown (void)
 {
-	if (!ices_config.reencode || ices_reencode_initialized == 0)
-		return;
+  if (ices_reencode_initialized)
+    lame_close (ices_reencode_flags);
 }
 
 /* reencode buff, of len buflen, put max outlen reencoded bytes in outbuf */
@@ -130,7 +108,7 @@ ices_reencode_reencode_chunk (unsigned char *buff, int buflen, unsigned char *ou
 	int decode_ret = lame_decode ((char *)buff, buflen, left, right);
 
 	if (decode_ret > 0)
-		return lame_encode_buffer (&ices_reencode_flags, left, right, decode_ret, (char *)outbuf, outlen);
+		return lame_encode_buffer (ices_reencode_flags, left, right, decode_ret, (char *)outbuf, outlen);
 	return decode_ret;
 }
 
@@ -138,7 +116,7 @@ ices_reencode_reencode_chunk (unsigned char *buff, int buflen, unsigned char *ou
 int
 ices_reencode_flush (unsigned char *outbuf, int maxlen)
 {
-	int ret = lame_encode_finish (&ices_reencode_flags, (char *)outbuf, maxlen);
+	int ret = lame_encode_flush_nogap (ices_reencode_flags, (char *)outbuf, maxlen);
 	return ret;
 }
 #endif
