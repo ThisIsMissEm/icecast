79e0374a8ee4c65bebd3f990762fd37f5fcdb70b
brendan <brendan@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2002-04-29 03:16:21 +0000

    Allow perl module to load even when ices_init is missing
    Allow perl module to be reloaded. This didn't work when I was using
    "-M<mod> -e 0" as the argv. After much fruitless head-bashing, the
    solution (at least here) is simple: "-e 'use <mod>'". Don't ask me
    why...
    
    next up, ruby scripting support :)

--- icecast/trunk/ices0/trunk/src/playlist/pm_perl.c
+++ icecast/trunk/ices0/trunk/src/playlist/pm_perl.c
@@ -1,7 +1,7 @@
 /* playlist_perl.c
  * - Interpreter functions for perl
  * Copyright (c) 2000 Chad Armstrong, Alexander Haväng
- * Copyright (c) 2001 Brendan Cully
+ * Copyright (c) 2001-2 Brendan Cully
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -48,6 +48,7 @@ extern void boot_DynaLoader ();
 static int playlist_perl_get_lineno (void);
 static char* playlist_perl_get_next (void);
 static char* playlist_perl_get_metadata (void);
+static int playlist_perl_reload (void);
 static void playlist_perl_shutdown (void);
 
 static int pl_perl_init_perl (void);
@@ -67,11 +68,15 @@ ices_playlist_perl_initialize (playlist_module_t* pm)
   pm->get_metadata = playlist_perl_get_metadata;
   pm->get_lineno = playlist_perl_get_lineno;
   pm->shutdown = playlist_perl_shutdown;
+  pm->reload = playlist_perl_reload;
 
   if (pl_perl_init_perl () < 0)
     return -1;
 
-  if (pl_init_hook && (str = pl_perl_eval (pl_init_hook))) {
+  if (!pl_init_hook)
+    return 1;
+
+  if ((str = pl_perl_eval (pl_init_hook))) {
     ret = atoi (str);
     ices_util_free (str);
   }
@@ -114,6 +119,14 @@ playlist_perl_get_metadata (void)
   return NULL;
 }
 
+static int
+playlist_perl_reload (void)
+{
+  playlist_perl_shutdown ();
+
+  return pl_perl_init_perl ();
+}
+
 static void
 playlist_perl_shutdown (void)
 {
@@ -141,23 +154,23 @@ xs_init (void)
 static int
 pl_perl_init_perl (void)
 {
-  static char *my_argv[5] = { "", "-I" ICES_MODULEDIR, NULL, "-e", "0" };
+  static char *my_argv[4] = { "", "-I" ICES_MODULEDIR, "-e", NULL };
   static char module_space[255];
 
-  snprintf (module_space, sizeof (module_space), "-M%s",
-	    ices_config.pm.module);
-  my_argv[2] = module_space;
-
-  ices_log_debug ("Importing Perl module %s", my_argv[2]);
-
-  if((my_perl = perl_alloc()) == NULL) {
+  if((my_perl = perl_alloc ()) == NULL) {
     ices_log_debug ("perl_alloc() error: (no memory!)");
     return -1;
   }
 
-  perl_construct(my_perl);
+  snprintf (module_space, sizeof (module_space), "use %s",
+	    ices_config.pm.module);
+  my_argv[3] = module_space;
 
-  if (perl_parse(my_perl, xs_init, 5, my_argv, NULL)) {
+  perl_construct (my_perl);
+
+  ices_log_debug ("Importing perl module: %s", my_argv[3]+4);
+
+  if (perl_parse(my_perl, xs_init, 4, my_argv, NULL)) {
     ices_log_debug ("perl_parse() error: parse problem");
     return -1;
   }
@@ -185,10 +198,11 @@ pl_perl_init_perl (void)
 static void
 pl_perl_shutdown_perl (void)
 {
-	if (my_perl != NULL){
-		perl_destruct(my_perl);
-		perl_free(my_perl);
-	}
+  if (my_perl != NULL){
+    perl_destruct(my_perl);
+    perl_free(my_perl);
+    my_perl = NULL;
+  }
 }
 
 
@@ -220,7 +234,7 @@ pl_perl_eval (const char *functionname)
 	/* Check for errors in execution */
 	if (SvTRUE (ERRSV)) {
 	  STRLEN n_a;
-	  ices_log_debug ("Perl error: %s", SvPV (ERRSV, n_a));
+	  ices_log_debug ("perl error: %s", SvPV (ERRSV, n_a));
 	  (void) POPs;
 	} else if (retcount) {
 	  /* we're calling strdup here, free() this later! */
