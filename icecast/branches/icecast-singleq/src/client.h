90e5e76bca62cf6f8025115a90aef148f58614e1
karl <karl@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-08-18 13:20:42 +0000

    make listening clients and the burst on connect handler affect buffer reference counts, this allows for the queues to be shrunk, removing unneeded data.

--- icecast/branches/icecast-singleq/src/client.h
+++ icecast/branches/icecast-singleq/src/client.h
@@ -56,5 +56,6 @@ void client_send_401(client_t *client);
 void client_send_403(client_t *client);
 void client_send_400(client_t *client, char *message);
 int client_send_bytes (client_t *client, const void *buf, unsigned len);
+void client_set_queue (client_t *client, refbuf_t *refbuf);
 
 #endif  /* __CLIENT_H__ */
