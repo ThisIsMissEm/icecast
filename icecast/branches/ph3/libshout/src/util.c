6bdf41f8af89261907f925b640d9af34e818aecd
ph3-der-loewe <ph3-der-loewe@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2012-11-21 14:14:03 +0000

    corrected copyright and license statements

--- icecast/branches/ph3/libshout/src/util.c
+++ icecast/branches/ph3/libshout/src/util.c
@@ -1,6 +1,7 @@
 /* util.c: libshout utility/portability functions
  *
- *  Copyright (C) 2002-2003 the Icecast team <team@icecast.org>
+ *  Copyright (C) 2002-2003 the Icecast team <team@icecast.org>,
+ *  Copyright (C) 2012      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
